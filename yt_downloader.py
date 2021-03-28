bc616ffd4dd0e2c4cf579bbd7f31d50d6c0b7789
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-28 13:44:44 -0400

    Doing connection function with requests and bs4

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,8 +1,29 @@
+import requests
 from bs4 import BeautifulSoup
 
 
+def set_connection(search_query):
+    url = f'https://www.youtube.com/results?search_query={search_query}'
+    
+    try:
+        response = requests.get(url)
+    except:
+        exit(0)
+
+    if response.status_code == 200:
+        content = response.text
+        soup = BeautifulSoup(content,'html.parser')
+        # print(soup.prettify())
+        # yt-simple-endpoint style-scope ytd-video-renderer
+        return soup
+    else:
+        print('Can not stablish connection with YouTube')
+        exit(0)
+
+
+
 def main():
-    pass
+    set_connection(search_query='react')
 
 
 if __name__ == '__main__':
