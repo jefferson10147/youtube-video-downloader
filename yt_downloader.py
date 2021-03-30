4dc1036915c7e969b5d029244868302e0cc79890
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-29 21:51:11 -0400

    Making a function that generate a dict with videos info

--- yt_downloader.py
+++ yt_downloader.py
@@ -10,6 +10,7 @@ API_TOKEN = parser['app']['api_token']
 
 
 def set_connection(search_query):
+    search_query = search_query.replace(' ','%20')
     url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&key={API_TOKEN}&type=video&q={search_query}'
     
     try:
@@ -25,13 +26,25 @@ def set_connection(search_query):
         exit(0)
 
 
-def get_videos_urls(soup):
-    pass
+def get_videos_urls(json_response):
+    base_url = 'https://www.youtube.com/watch?v='
+    videos_info = json_response['items']
+    urls = {}
+
+    for video_info in videos_info:
+        video_key = video_info['id']['videoId']
+        video_title = video_info['snippet']['title']
+        video_title = video_title.replace('\u200b', ' ')
+        channel_title = video_info['snippet']['channelTitle']
+        urls[video_key] = f'{channel_title} --> {video_title}'
+
+    return urls
 
 
 def main():
-    json_response = set_connection(search_query='react')
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    json_response = set_connection(search_query='auron reacciona cine argentino')
+    urls = get_videos_urls(json_response)
+    print(json.dumps(urls, indent=4, sort_keys=True))
     #get_videos_urls(soup)
     pass
 
