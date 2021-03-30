c294f35f2262f54ed98afe877de318277ad5cebc
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-29 21:27:52 -0400

    Using youtube api

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,9 +1,16 @@
 import requests
+import json
+import configparser
 from bs4 import BeautifulSoup
 
 
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+API_TOKEN = parser['app']['api_token']
+
+
 def set_connection(search_query):
-    url = f'https://www.youtube.com/results?search_query={search_query}'
+    url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&key={API_TOKEN}&type=video&q={search_query}'
     
     try:
         response = requests.get(url)
@@ -11,20 +18,21 @@ def set_connection(search_query):
         exit(0)
 
     if response.status_code == 200:
-        content = response.text
-        soup = BeautifulSoup(content,'html.parser')
-        # print(soup.prettify())
-        # yt-simple-endpoint style-scope ytd-video-renderer
-        return soup
+        return response.json()
     else:
         print('Can not stablish connection with YouTube')
         exit(0)
 
 
+def get_videos_urls(soup):
+    pass
+
 
 def main():
-    set_connection(search_query='react')
-
+    json_response = set_connection(search_query='react')
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+    #get_videos_urls(soup)
+    pass
 
 if __name__ == '__main__':
     main()
