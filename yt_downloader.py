8894112f10ecc9147cb7ccb6d3d089c8ea1298de
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-04-02 17:50:32 -0400

    First version of download bar

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,4 +1,6 @@
 import os
+import time
+import threading
 import requests
 import configparser
 import argparse
@@ -115,14 +117,45 @@ def download_video(url):
     video = YouTube(url)
     stream = video.streams.first()
     title = video.title
-    video_size = video.filesize
-    # start thread
+    video_size = int(stream.filesize)
+    thread = threading.Thread(target=progress_bar, args=(title, video_size))
+    thread.start()
     stream.download('./downloads/')
 
 
 def progress_bar(video_name, video_size):
-    # os.stat(f'./downloads/{video_name}.mp4')
-    pass
+    route = f'./downloads/{video_name}.mp4'
+    percentage = 0.1
+    partition_sizes = list()
+    total_partitions = 10
+    partitions_recovered = 1
+    video_size_mb = round(video_size / 1000000, 2)
+
+    for i in range(1, total_partitions):
+        partition_sizes.append(round(video_size * percentage, 1))
+        percentage = round(percentage + 0.1, 1)
+
+    while True:
+        time.sleep(0.1)
+        try:
+            current_size = os.stat(route).st_size
+        except FileNotFoundError:
+            continue
+
+        if current_size >= video_size:
+            break
+
+        try:
+            if current_size >= partition_sizes[0]:
+                partition_sizes.pop(0)
+                partitions_left = total_partitions - partitions_recovered
+                current_size_mb = round(current_size / 1000000, 2)
+                download_left = f'{current_size_mb} / {video_size_mb} mb'
+                print('[', '##'*partitions_recovered, '**' *
+                      partitions_left, ']', download_left, end='\r')
+                partitions_recovered += 1
+        except IndexError:
+            break
 
 
 def main():
